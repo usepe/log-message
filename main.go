afba842d8111e3a143f740e6ad22a35bf56ca10b
Uriel Sepe <uriel.sepe@etermax.com>
2020-11-29 08:10:56 -0300

    message reader

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	cstr := os.Getenv("CONNECTION_STRING")
+	qname := os.Getenv("QUEUE_NAME")
+
+	conn, err := amqp.Dial(cstr)
+
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		qname, // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	log.Printf("Init...")
+
+	select {
+	case d := <-msgs:
+		log.Printf("Received a message: %s", d.Body)
+	}
+
+	log.Printf("Done.")
+}
